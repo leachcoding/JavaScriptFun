9ee26b890b29b8808c0c9c405d7719270cbbfc18
Jay Leach <leachcoding@gmail.com>
2020-02-27 15:28:22 -0500

    Classes.js created starting functions.js

---
+++ challenges/classes.js
@@ -0,0 +1,5 @@
+// Copy and paste your prototype in here and refactor into class syntax.
+
+// Test the volume and surfaceArea methods using console.log. If you're unsure what the actual volumes and surfaceAreas should be, Google is once again your best friend.
+
+// STRETCH: Extend the base class PentagonalPyramid with a subclass called Pyramid. Find the formulas for volume and surfaceArea for regular pyramids and create those methods using dimension properties from PentagonalPyramid. Test your work by logging out the volume and surfaceArea. 
