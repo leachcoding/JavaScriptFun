2d986949855042234dd50d20e985b629bfc1c8e1
Jay Leach <leachcoding@gmail.com>
2020-02-27 16:48:33 -0500

    finished

---
+++ challenges/functions.js
@@ -0,0 +1,22 @@
+
+// Callbacks
+
+// Step 1: Create a higher-order function that will accept 3 parameters. The first two can take any argument. The last is your callback. This function should return your callback that passed the first two parameters as its argument.
+
+/* Step 2: Create several functions.
+The first will return the sum of two numbers.
+The second will return the product of two numbers.
+The third will return the modulus of the two numbers.
+The fourth will return the quotient of the two numbers.
+The fifth will return the square root of the two numbers.
+The sixth will accept a first and last name and return 'Hey, firstName lastName, youre a wicked cool dev'.
+The seventh will return whether the firstString is included the secondString.
+The eigth will return whether the firstNumber is greater than the secondNumber.
+The ninth will return whether the firstNumber is less than the secondNumber.
+The tenth will return whether the firstNumber is greater than or equal to the secondNumber.
+The eleventh will return whether the firstNumber is less than or equal to the secondNumber.
+The twelvth will return whether the firstNumber is equivalent to the secondNumber.
+The thirteenth will return a string 'Hey the number is firstNumber' if firstNumber is greater than the secondNumber. If it isn't, return a string 'Hey the number is secondNumber'
+*/
+
+// Step 3: Check your work using console.log and invoke your higher order function.
---
+++ challenges/objects-arrays.js
@@ -0,0 +1,66 @@
+// Objects
+
+// Given the following about information about animals, create some objects: name, diet, weight, length, area
+
+// Object 1 -- dog, omnivorous, 25lb, 2ft, worldwide
+
+// Object 2 -- tiger, carnivorous, 400lb, 6ft, india
+
+// Object 3 -- koala, herbivorous, 40lb, 3ft, australia
+
+/* Using the console, log the following
+
+How much the dog weighs
+What the diet of a tiger is
+Where are koalas located
+What is the length of all 3 objects
+
+*/
+
+// Create a new bark method for the dog. When called, return "Woof!"
+// Create a new hunt method for the tiger. When called, if Math.random(10) is greater than 7, return "Tiger hunt successful" and add 5lb to the weight. If it is not, return "Tiger goes hungry" and subtract 5lbs to the weight.
+// Create a new climb method for the koala. When called, return "Eucalyptus is great"
+
+
+// Arrays
+
+// Given this array of game characters, complete the following tasks
+
+const gameCharacters = [
+  {id: 1, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 2, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 3, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 4, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 5, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 6, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 7, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 8, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 9, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]},
+  {id: 10, name: "Mario", game: "Super Mario World", mission: "Save the Princess", enemy: "Bowser", subEnemies: ["Koopa", "Goomba", "BowserJr"]}
+];
+
+// Step 1: Create a new array that will contain all games. Once you have it made, sort it ascending[A-Z]. Log the result. Solve two ways:
+
+// For Loop
+
+// Map or forEach
+
+
+// Step 2: Create a new array that will contain the name and mission of each character.Log the result. Solve two ways:
+
+// For Loop
+
+// Map or forEach
+
+
+// Step 3: Find out how many sub enemies are in each character. Display the number as well as the array. Solve two ways
+
+// For Loop
+
+// Map or forEach
+
+// Step 4: Find out how many missions include the term "save" (NOTE CAPITALIZATION SHOULD BE NULLIFIED IT SHOULDNT MATTER IF THE TERM HAS A CAP OR NOT). Log that result of the missions that do. Solve two ways:
+
+// For Loop
+
+// Map or forEach or filter
