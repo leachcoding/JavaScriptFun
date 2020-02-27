94c9fa34fd7b1df314470c00295c195a94e1f1a8
Jay Leach <leachcoding@gmail.com>
2020-02-27 17:34:49 -0500

    final edit

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -2,7 +2,7 @@
 
 // Task: You are to build a pentagonal pyramid that can return values for its volume as well as its surface areas. Follow the steps to achieve this.
 
-// Step 1: Base Constructor -- You should create a constructor function names PentagonalPyramid that will accept properties for its length, width, and heights.
+// Step 1: Base Constructor -- You should create a constructor function names PentagonalPyramid that will accept properties for its base edge, and heights.
 
 // Step 2: Volume Method -- You should create a prototype for PentagonalPyramid that returns the volume of a pentagonal pyramid. Curious about the formula??? Use Google (A DEVELOPERS BEST FRIEND)
 
@@ -10,4 +10,4 @@
 
 // Step 4: New Object -- You should create a new object that uses PentagonalPyramid. Give it the length, width, and height.
 
-// Test your volume and surfaceArea methods here using console.log 
+// Test your volume and surfaceArea methods here using console.log
